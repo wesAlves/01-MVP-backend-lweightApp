38e8f1114cc0d2593b08f6c999b2acb4cc47e616
Wes <wal2.designer@gmail.com>
2022-06-13 16:52:32 +0100

    find out how to fiz the object

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,5 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../database";
-import {v4 as uuidv4} from 'uuid'
 import { User } from "../model/User";
 
 export const userRoute = Router();
@@ -18,11 +17,14 @@ const users: User[] = []
 userRoute.post('/user', (request: Request, response: Response) => {
     const {name, email, password} = request.body;
 
-    const newUser: User = {name, email, password, created_at: new Date(), id: uuidv4() }
+    // const newUser: User = {name, email, password, created_at: new Date() }
+    const newUser = new User();
+
+    newUser.create()
 
     users.push(
         newUser
     )
 
-    return response.status(201).json({...newUser})
+    return response.status(201).json(newUser)
 })
