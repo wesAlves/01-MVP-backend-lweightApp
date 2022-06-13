3141365beee8c4af4015328a5911ef33a3a6eaf6
Wes <wal2.designer@gmail.com>
2022-06-13 16:43:01 +0100

    wip: create model

---
+++ src/model/User.ts
@@ -0,0 +1,10 @@
+class User{
+    id: string;
+    name: string;
+    email: string;
+    created_at: Date;
+    password: string;
+
+}
+
+export {User}
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,20 +1,28 @@
 import { Request, Response, Router } from "express";
 import { AppDataSource } from "../database";
+import {v4 as uuidv4} from 'uuid'
+import { User } from "../model/User";
 
 export const userRoute = Router();
 
-userRoute.get('/user', (request: Request, response: Response) =>{
-    const dataSource = AppDataSource.manager.find('John')
+const users: User[] = []
 
-    dataSource
+// userRoute.get('/user', (request: Request, response: Response) =>{
+//     const dataSource = AppDataSource.manager.find('John')
+
+//     dataSource
     
-    return response.json({user: "John Doe"})
-})
+//     return response.json({user: "John Doe"})
+// })
 
 userRoute.post('/user', (request: Request, response: Response) => {
     const {name, email, password} = request.body;
 
-    const newUser = {name, email, password, createdAt: new Date()}
+    const newUser: User = {name, email, password, created_at: new Date(), id: uuidv4() }
+
+    users.push(
+        newUser
+    )
 
     return response.status(201).json({...newUser})
 })
